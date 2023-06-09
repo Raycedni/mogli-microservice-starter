9d29ff0fd2921b57fb2b058a04d300bc29329a88
Raycedni <lukas.loeffler95@gmail.com>
2023-06-09 22:57:47 +0200

    add hibernate support and provide default SecurityConfig

--- core/src/main/java/com/mogli/microservicebase/security/ParentSecurityConfig.java
+++ core/src/main/java/com/mogli/microservicebase/security/ParentSecurityConfig.java
@@ -3,6 +3,7 @@ package com.mogli.microservicebase.security;
 import com.mogli.microservicebase.security.jwt.JWTDecoder;
 import com.mogli.microservicebase.security.jwt.JwtAuthConverter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,41 +20,44 @@ import java.util.Arrays;
 
 @RequiredArgsConstructor
 @Configuration
+@AutoConfiguration
 @EnableWebSecurity
-public class SecurityConfig {
+public class ParentSecurityConfig {
 
-    private final KeycloakLogoutHandler keycloakLogoutHandler;
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return buildDefaultFilterChain(http).build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(Arrays.asList("https://example.com"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 
     @Bean
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
         return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
     }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    protected HttpSecurity buildDefaultFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests()
                 .requestMatchers("/api-docs/**", "").permitAll()
                 .anyRequest().authenticated();
-        http.oauth2Login()
-                .and()
-                .logout()
-                .addLogoutHandler(keycloakLogoutHandler)
-                .logoutSuccessUrl("/");
+//        http.oauth2Login()
+//                .and()
+//                .logout()
+//                .addLogoutHandler(keycloakLogoutHandler)
+//                .logoutSuccessUrl("/");
         http.oauth2ResourceServer((oauth2ResourceServer) ->
                 oauth2ResourceServer.jwt((jwt) -> {
                     jwt.jwtAuthenticationConverter(new JwtAuthConverter());
                     jwt.decoder(new JWTDecoder());
                 }));
-        return http.build();
-    }
-
-    @Bean
-    CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-//        configuration.setAllowedOrigins(Arrays.asList("https://example.com"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE", "PATCH"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+        return http;
     }
 }
